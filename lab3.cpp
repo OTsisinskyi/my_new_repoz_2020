f61ddef9e664df5c09ab322c607925dc3d87bdbf
OTsisinskyi <alextsisinsky@gmail.com>
2020-11-23 11:45:27 +0200

    my first commit

--- lab3.cpp
+++ lab3.cpp
@@ -1,64 +1,44 @@
 ﻿#include <stdio.h>
-#include <stdio.h>
-#include <math.h>
+#define _CRT_SECURE_NO_WARNINGS
 
-void tab1()
-{
-    int a = 2, b = 5;
-    double h = 0.2;
-    /*
-      printf("a:\n");
-      scanf_s("&lf", &a);
+#define numbers 81
 
-      printf("b:\n");
-      scanf_s("&lf", &b);
+int main() {
+    int array[81];
+    int k;
+    int i, j, n;
 
-      printf("h:\n");
-      scanf_s("&lf", &h);
-    */
-    for (double x = a; x <= b; x += h)
-    {
-        if (x < 3)
-        {
-            printf("%fl %f\n", log10(log(x) + (sin(x))), x);
-        }
-        else if (x < 4)
-        {
-            printf("%fl %f\n", 1 / tan(x + log(x)), x);
-        }
-        else
-        {
-            printf("%fl %f\n", 1 / (16 - pow(x, 2)), x);
-        }
+    int a[9][9] = { 0 };
+
+    for (k = 0; k < numbers; k++) {
+        scanf_s("%d\n", &array[k]);
     }
     printf("\n");
-    
-}
-
-void tab2()
-{
-    double sum, m = 20, add, n;
-
-    for (double i = 0; i <= 0.5; i += 0.05)
-    {
-        sum = -(m * i);// залежність наступного від попереднього
-        add = sum;
-        n = 2;
-        do
-        {
-            add = pow(-1,n)((add * (m - n + 1) * i) / n);
-            sum += add;
-            n++;
-            // printf("%.2f", n);
-        } while fabs(add >= 0.001);
-        printf("x = %.3f y = %.3f\n", i, sum + 1);
+    void change_array(); {
+        k = 0;
+        for (n = 0; n < 9; n++) {
+            for (i = 0; i <= n; i++) {
+                a[8 - (n - i)][i] = array[k];
+                k++;
+            }
+        }
+        k = numbers - 1;
+        for (n = 0; n < 8; n++) {
+            for (i = 0; i <= n; i++) {
+                a[8 - i][n - i] = array[k];
+                k--;
+            }
+        }
     }
-}
-int main()
-{
-    tab1();
+    void print_array(); {
+        for (i = 0; i < 9; i++) {
+            for (j = 0; j < 9; j++) {
+                printf("%02d ", a[i][j]);
 
-    tab2();
+            }
+            printf("\n");
+        }
 
-    return 0;
+    }
+
 }
